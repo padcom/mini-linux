589db99de2ba087c0e322d560ee798dda20a2e30
Matthias Hryniszak <padcom@gmail.com>
2022-12-08 04:55:53 +0100

    Fixed initialization problem

--- init-target.sh
+++ init-target.sh
@@ -4,13 +4,16 @@ rm -f
 rm -f busybox
 rm -rf busybox-1.13.0
 
-#wget -c https://busybox.net/downloads/busybox-1.35.0.tar.bz2
+wget -c https://busybox.net/downloads/busybox-1.35.0.tar.bz2
 tar -xjf busybox-1.35.0.tar.bz2
 ln -s busybox-1.35.0 busybox
 
 cp busybox.config busybox/.config
 
-mkdir target
+mkdir -p target
 cp -a root/* target/
 
 ./build-target.sh
+cd target
+ln -s sbin/init init
+cd ..
